3bf97f3ed72814e5f280327cc94656051b507e7f
wunbo21 <43051954+wunbo21@users.noreply.github.com>
2019-04-05 22:01:08 -0300

    Delete aula_05.js

--- aula_05.js
+++
@@ -1,7 +0,0 @@
-//Vetor
-//Método construtor
-
-var frutas = new Array("Maça","Pera","Tody","Morango");
-document.write(frutas);
-
-document.write("<br>Na posição 1 o elemento é\u00A0"+frutas[1]); //Escolhe a posição
